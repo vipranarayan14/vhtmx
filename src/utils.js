609bbf2dbe5cfd33906f6201a1ceee23f1e96c40
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-07-15 19:38:07 +0530

    move 'readFile', 'writeFile' and 'mkdirp' functions to a seperate 'utils' module.

---
+++ src/utils.js
@@ -0,0 +1,55 @@
+import { readFile as _readFile } from 'fs';
+import { writeFile as _writeFile } from 'fs';
+import mkdirp from 'mkdirp';
+
+export const logError = errMsg => console.error(errMsg); //eslint-disable-line no-console
+
+export const makeDirs = outputDirPath =>
+
+  new Promise((resolve, reject) =>
+
+    mkdirp(outputDirPath, mkdirpErr => {
+
+      if (mkdirpErr) {
+
+        reject(mkdirpErr);
+
+      }
+
+      resolve();
+
+    })
+
+  );
+
+export const readFile = filePath => new Promise((resolve, reject) =>
+
+  _readFile(filePath, 'utf8', (err, data) => {
+
+    if (err) {
+
+      return reject(err);
+
+    }
+
+    return resolve(data);
+
+  })
+
+);
+
+export const writeFile = (filePath, data) => new Promise((resolve, reject) =>
+
+  _writeFile(filePath, data, 'utf8', err => {
+
+    if (err) {
+
+      reject(err);
+
+    }
+
+    resolve();
+
+  })
+
+);
